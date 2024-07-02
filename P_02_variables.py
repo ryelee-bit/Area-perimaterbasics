7fb6b3bf9620227c41ab0896fbe85a001cc01817
ryelee-bit <ryemcki@chbc.school.nz>
2024-07-03 09:38:47 +1200

    variable basics

---
+++ P_02_variables.py
@@ -0,0 +1,17 @@
+# Code to learn about variables...
+
+# variables can be a string (note the speech marks)
+greeting = "chur bro"
+
+# variables can be a number
+fav_num =  21
+
+# variables can be boolean (Frue / False)
+love_programing = True
+
+print("chur bro")
+print(fav_num)
+print(love_programing)
+
+# This variable has a problem!
+my_name = "Roo"
